4754d60741e87ae0f311e6d9fd7e3ff6db402b62
pengboci <80105235+pengboci@users.noreply.github.com>
2025-09-21 17:46:46 +0800

    Update aggregation_setting_unique.py

--- codes/aggregation_setting_unique.py
+++ codes/aggregation_setting_unique.py
@@ -284,10 +284,7 @@ def sample_multi_entities(graph):
         if edge[1] == relation:
             st_node.append(edge[0])
 
-    num = random.randint(3, 7)
-    entities = random.sample(st_node, min(num, len(st_node)))
-
-    return entities
+    return st_node
 
 output_file = 'aggregation_setting_unique.jsonl'
 graph_file = ['/graphs/new_graphs.jsonl']
